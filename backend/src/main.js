4cb0a1e777b4bf2e2675ffabcb21cdf2bb01088f
Your Name <gilbertozioma0@gmail.com>
2024-01-29 00:20:59 +0100

    imported store to main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,7 +1,11 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
-import App from './App.vue';
+import store from './store';
 import './style.css';
 
-createApp(App).mount('#app');
+import App from './App.vue';
+
+createApp(App)
+    .use(store)
+    .mount('#app')
