1af75535bf96408b7e62bb36f6762c463d54b0bf
Your Name <gilbertozioma0@gmail.com>
2024-02-04 11:02:18 +0100

    imported router

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,10 +2,12 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import store from './store';
+import router from './router';
 import './style.css';
 
 import App from './App.vue';
 
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
